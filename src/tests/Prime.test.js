fad0135eaa4b5a6ad8aa05c711d149f3c7b2693c
bergurijohansen <bergurij@gmail.com>
2019-03-27 21:21:25 +0000

    more fail

--- src/tests/Prime.test.js
+++ src/tests/Prime.test.js
@@ -1,13 +1,13 @@
 // import React from "react";
 
 it("4 + 4 = 8", () => {
-  const res = 4 + 4;
+  const res = 3 + 4;
 
   expect(res).toEqual(8);
 });
 
 it("value is true", () => {
-  const value = false;
+  const value = true;
 
   expect(value).toEqual(true);
 });
