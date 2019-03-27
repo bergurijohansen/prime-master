aac5eda8c0c03133c616db9a802849f8c38496cf
Bergur Johansen <bergurij@gmail.com>
2019-03-27 23:03:50 +0000

    fail

--- src/tests/Prime.test.js
+++ src/tests/Prime.test.js
@@ -6,6 +6,12 @@ it("4 + 4 = 8", () => {
   expect(res).toEqual(8);
 });
 
+it("4 + 4 = 9, should Fail", () => {
+  const res = 4 + 4;
+
+  expect(res).toEqual(9);
+});
+
 it("value is true", () => {
   const value = true;
 
