e970f9da96ff10ba7526a27cece232b2e44d304f
Bergur Johansen <bergurij@gmail.com>
2019-03-27 22:59:42 +0000

    Updates

---
+++ precache-manifest.d58b06ac8ae4a5f8a7a806be2bd86e02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e607bfffcf7bffcb0c70",
+    "url": "/prime-master/static/js/runtime~main.c114c473.js"
+  },
+  {
+    "revision": "4268e4d3335980a127a6",
+    "url": "/prime-master/static/js/main.82efb2a0.chunk.js"
+  },
+  {
+    "revision": "38e9280959dd5f0d5c78",
+    "url": "/prime-master/static/js/2.88ea1fd6.chunk.js"
+  },
+  {
+    "revision": "4268e4d3335980a127a6",
+    "url": "/prime-master/static/css/main.aee335c7.chunk.css"
+  },
+  {
+    "revision": "409b5561142b6a1af91bdb09c956b98e",
+    "url": "/prime-master/index.html"
+  }
+];
